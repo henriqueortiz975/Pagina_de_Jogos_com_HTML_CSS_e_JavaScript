f0e8aa12d2a92fb1aa36e07f8eb79b642cdb953f
henriqueortiz975 <henriqueortiz3090@gmail.com>
2026-05-19 16:20:18 -0300

    Arrumando algumas coisas que ainda estão faltando

--- script.js
+++ script.js
@@ -3,16 +3,16 @@ const botoes = document.querySelectorAll(".Botao");
 botoes.forEach(function(Botao){
 
     Botao.addEventListener("click", function(){
-        if(Botao.id === "1"){
+        if(Botao.id === "Jogo1"){
             // Adcionar o jogo genius
             window.location.href = "genius/index.html";
-        }else if(Botao.id === "2"){
+        }else if(Botao.id === "Jogo2"){
             // Adcionar o Jogo da velha
             window.location.href = "jogo-da-velha/index.html";
-        }else if(Botao.id === "3"){
+        }else if(Botao.id === "Jogo3"){
             // Adcionar o Jogo de Adivinhação de Número
             window.location.href = "adivinhacao/index.html";
-        }else if(Botao.id === "4"){
+        }else if(Botao.id === "Jogo4"){
             // Adcionar o Jogo de pedra, papel e tesoura
             window.location.href = "pedra-papel-tesoura/index.html";       
         }else{
